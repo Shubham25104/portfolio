b96b1536ef7fe61d90d2373bfca0955895feb0a2
Shubham25104 <Shubham25104@gmail.com>
2025-04-20 20:50:18 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* =========================== typing animation ==================== */
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed('.typing', {
+      strings: [" ","Web Developer", "Software Engineer", "Freelancer"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+    });
+  });
